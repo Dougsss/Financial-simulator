c4898323879844a36faf475990da4a5ad64403c9
Doug <86627627+Dougsss@users.noreply.github.com>
2022-02-13 21:52:52 -0300

    Add arquivos faltantes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import Header from './components/Header';
 import Simulator from './components/Simulator';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className='flex flex-row justify-center flex-grow h-screen '>
-      <div className='w-11/12 bg-gray-100 h-5/6 mt-7'>
-        <Header/>
-        <div className='px-8'>
-          <Simulator/>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { indicators: [] }
+  }
+  componentDidMount() {
+    fetch('http://localhost:3000/indicadores')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ indicators: data })
+      })
+  }
+  render() {
+    return (
+      <div className='flex flex-row justify-center flex-grow h-screen '>
+        <div className='w-11/12 bg-gray-100 h-5/6 mt-7'>
+          <Header />
+          <div className='px-8'>
+            <Simulator indicators={this.state.indicators} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+export { App };
