753c39592c86b0085ad9d9269f8392f9adefac48
Doug <86627627+Dougsss@users.noreply.github.com>
2022-02-17 11:54:58 -0300

    stilização dos button, inputs e responsividade

--- src/components/Simulator/index.js
+++ src/components/Simulator/index.js
@@ -48,21 +48,21 @@ export default function Simulator({ indicators }) {
     }
   })
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-col mt-8 lg:flex-row">
       <form className="flex flex-col w-1/2" onSubmit={handleSubmit(onSubmit)}>
-        <h2 className="mb-4 font-bold">Simulador</h2>
-        <div className="flex flex-row gap-5">
+        <h2 className="mb-5 text-xl font-bold">Simulador</h2>
+        <div className="flex flex-row gap-12">
           <div className="flex flex-col">
-            <div className="flex justify-between flex-grow">
+            <div className="flex justify-between flex-grow mb-2 font-extralight">
               <p>Rendimento</p>
               <span>!</span>
             </div>
             {/* Este bloco contém buttons que vai selecionar os estados para o calculos */}
-            <div className="flex flex-row border-2 rounded-lg">
-              <button type="button" className={`flex flex-auto p-2 border-r-2 ${incomeType === 'bruto' ? 'bg-orange-500 text-white' : ''}`} onClick={() => { setIncomeType('bruto') }}>Bruto</button>
-              <button type="button" className={`flex flex-auto p-2 border-r-2 ${incomeType === 'liquido' ? 'bg-orange-500 text-white' : ''}`} onClick={() => { setIncomeType('liquido') }}>Liquido</button>
+            <div className="flex flex-row mb-6 border-2 border-black rounded-lg">
+              <button type="button" className={`flex flex-auto p-2 ${incomeType === 'bruto' ? 'bg-orange-500 text-white rounded-lg border-r-2 border-black' : ''}`} onClick={() => { setIncomeType('bruto') }}>Bruto</button>
+              <button type="button" className={`flex flex-auto p-2 ${incomeType === 'liquido' ? 'bg-orange-500 text-white rounded-lg border-l-2 border-black' : ''}`} onClick={() => { setIncomeType('liquido') }}>Líquido</button>
             </div>
-            <label>Aporte Inicial</label>
+            <label className="mb-2 font-extralight">Aporte Inicial</label>
             {/* Adicionei um controlador para "controlar" as regras e o que vai ser renderizado */}
             <Controller
               control={control}
@@ -81,27 +81,27 @@ export default function Simulator({ indicators }) {
               )} />
             {/* Este bloco contem as  regras aplicadas aos erros dos campos */}
             {errors.initialValue && <span className="text-red-500">Este compo é obrigatorio</span>}
-            <label>Prazo (em meses)</label>
-            <input autoComplete="off" type="text" {...register("deadLine", { required: true, min: 1 })} />
+            <label className="mt-5 mb-2 font-extralight">Prazo (em meses)</label>
+            <input className="border-b-2 border-gray-400 appearance-none" autoComplete="off" type="text" {...register("deadLine", { required: true, min: 1 })} />
             {errors.deadLine?.type === 'required' && <span className="text-red-500">Este compo é obrigatorio</span>}
             {errors.deadLine?.type === 'min' && <span className="text-red-500">Prazo tem que ser maior que 0</span>}
-            <label>IPCA (ao ano)</label>
-            <input autoComplete="off" type="text" {...register("ipca", { disabled: true })} />
+            <label className="mt-5 mb-2 font-extralight">IPCA (ao ano)</label>
+            <input className="mb-8 border-b-2 border-gray-400 appearance-none" autoComplete="off" type="text" {...register("ipca", { disabled: true })} />
             {/* Botão contendo a função de limpar os campos com dados */}
-            <button onClick={cleanFields} type="button" className="border-2 border-black border-solid rounded-lg">Limpar Campos</button>
+            <button onClick={cleanFields} type="button" className="font-bold border-2 border-black border-solid rounded-lg h-11">Limpar Campos</button>
           </div>
           <div className="flex flex-col">
-            <div className="flex justify-between flex-grow">
+            <div className="flex justify-between flex-grow mb-2 font-extralight">
               <p>Tipos de indexação</p>
               <span>!</span>
             </div>
             {/* Este bloco contém buttons que vai selecionar os estados para o calculos */}
-            <div className="flex flex-row border-2 rounded-lg">
-              <button type="button" className={`flex flex-auto p-2 border-r-2 ${indexType === 'pre' ? 'bg-orange-500 text-white' : ''}`} onClick={() => setIndexType('pre')}>PRÉ</button>
-              <button type="button" className={`flex flex-auto p-2 border-r-2 ${indexType === 'pos' ? 'bg-orange-500 text-white' : ''}`} onClick={() => setIndexType('pos')}>POS</button>
-              <button type="button" className={`flex flex-auto p-2 border-r-2 ${indexType === 'ipca' ? 'bg-orange-500 text-white' : ''}`} onClick={() => setIndexType('ipca')}>FIXADO</button>
+            <div className="flex flex-row mb-6 border-2 border-black rounded-lg">
+              <button type="button" className={`flex flex-auto p-2 ${indexType === 'pre' ? 'bg-orange-500 text-white rounded-lg border-r-2 border-black' : ''}`} onClick={() => setIndexType('pre')}>PRÉ</button>
+              <button type="button" className={`flex flex-auto p-2 ${indexType === 'pos' ? 'bg-orange-500 text-white rounded-lg border-x-2 border-black' : ''}`} onClick={() => setIndexType('pos')}>POS</button>
+              <button type="button" className={`flex flex-auto p-2 ${indexType === 'ipca' ? 'bg-orange-500 text-white rounded-lg border-l-2 border-black' : ''}`} onClick={() => setIndexType('ipca')}>FIXADO</button>
             </div>
-            <label>Aporte Mensal</label>
+            <label className="mb-2 font-extralight">Aporte Mensal</label>
             {/* Adicionei um controlador para "controlar" as regras e o que vai ser renderizado */}
             <Controller
               control={control}
@@ -119,35 +119,35 @@ export default function Simulator({ indicators }) {
               )} />
             {/* Este bloco contem as  regras aplicadas aos erros dos campos */}
             {errors.monthlyValue && <span className="text-red-500">Este compo é obrigatorio</span>}
-            <label>Rentabilidade (%)</label>
-            <input className="" autoComplete="off" type="number" {...register("profitability", { required: true, min: 0 })} />
+            <label className="mt-5 mb-2 font-extralight">Rentabilidade (%)</label>
+            <input className="border-b-2 border-gray-400 appearance-none" autoComplete="off" type="number" {...register("profitability", { required: true, min: 0 })} />
             {errors.profitability && <span className="text-red-500">Este compo é obrigatorio</span>}
-            <label>CDI (ao ano)</label>
-            <input autoComplete="off" type="text" {...register("cdi", { disabled: true })} />
-            <button type="submit" className="border-2 border-black border-solid rounded-lg">
+            <label className="mt-5 mb-2 font-extralight">CDI (ao ano)</label>
+            <input className="mb-8 border-b-2 border-gray-400 appearance-none" autoComplete="off" type="text" {...register("cdi", { disabled: true })} />
+            <button type="submit" className="font-bold bg-gray-300 border-2 border-black border-solid rounded-lg hover:text-white hover:bg-orange-500 h-11">
               Simular
             </button>
           </div>
         </div>
       </form>
       {/* Este bloco exibe os resultados dos valores com os parametros fornecidos pela API */}
-      <div className="flex flex-row w-1/2 gap-x-2">
-        <div className="flex flex-col gap-y-2">
-          <div className="p-5 bg-white">valor final bruto
+      <div className="flex flex-col w-1/2 mt-12 lg:flex-row gap-x-5">
+        <div className="flex flex-col gap-y-5">
+          <div className="p-5 bg-white">Valor final bruto
             <p>{result?.valorFinalBruto}</p></div>
-          <div className="p-5 bg-white">valor final liquido
+          <div className="p-5 bg-white">Valor final líquido
             <p>{result?.valorFinalLiquido}</p></div>
         </div>
-        <div className="flex flex-col gap-y-2">
-          <div className="p-5 bg-white">cota do ir
+        <div className="flex flex-col gap-y-5">
+          <div className="p-5 bg-white">Cota do IR
             <p>{result?.aliquotaIR}</p></div>
-          <div className="p-5 bg-white">total investido
+          <div className="p-5 bg-white">Total investido
             <p>{result?.valorTotalInvestido}</p></div>
         </div>
-        <div className="flex flex-col gap-y-2">
-          <div className="p-5 bg-white">pago ir
+        <div className="flex flex-col gap-y-5">
+          <div className="p-5 bg-white">Pago IR
             <p>{result?.valorPagoIR}</p></div>
-          <div className="p-5 bg-white">ganho liquido
+          <div className="p-5 bg-white">Ganho liquido
             <p>{result?.ganhoLiquido}</p></div>
         </div>
       </div>
